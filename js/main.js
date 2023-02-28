a85b94d1651d05d7d7b37763573e763df0b2fc5f
esteveleonardo@gmail.com <esteveleonardo@gmail.com>
2023-02-28 16:24:06 +0100

    primera parte y maquetada

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// recoger del input search
+// select 
+// dos paginas porque 
+const form = document.querySelector('form')
+const submit = document.querySelector('#submit')
+const grid = document.querySelector('.gridcontainer')
+
+document.body.addEventListener('click' ,(ev)=> {
+    const search = document.querySelector('#search').value;
+    const select = document.querySelector('#select').value;
+    ev.preventDefault();
+    if (ev.target.matches('#submit')) {
+        peticion(search, select);
+    }
+
+})
+
+const peticion = (search, select) => {
+    fetch(`https://api.pexels.com/v1/search?query=${search}&orientation=${select}`, {
+        method: 'GET',
+        headers: {
+          'authorization': 'G5Ojje39ZfUduWTOm2FOUUg9JYl9C18ode6hU4hB5IBEQv4Z2YOWJr1v'
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => validarPaginas(json));
+    }
+
+
+
+const validarPaginas = (resp) => {
+    paginas =Math.ceil (resp.total_results / resp.per_page);
+    pintar(resp, paginas);
+}
+
+const pintar = (resp, paginas)  => {
+    grid.innerHTML= '';
+    resp.photos.forEach((item, index)=>{
+        const div = document.createElement('DIV')
+        const img = document.createElement('IMG')
+        const url = resp.photos[index].src.medium
+        const alt = resp.photos[index].alt
+        div.classList.add('imgcontainer')
+        img.setAttribute('alt', alt)
+        img.setAttribute('src', url)
+        div.append(img)
+        grid.append(div)
+    })
+
+    
+}
+
+
